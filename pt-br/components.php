ee1f64238716eb6c89cf7f456dacdc81337e6ee8
Diego Canton de Brito <diegocantonbgi@gmail.com>
2021-12-19 14:53:26 -0300

    TRADUÇÃO COMPLETA

--- pt-br/components.php
+++ pt-br/components.php
@@ -2,7 +2,7 @@
 
 return [
     'notification-subscription-status' => [
-        'no-support' => 'Este navegador não suporta notificações',
+        'no-support' => 'Este navegador não suporta notificações!',
         'no-transport' => 'Para habilitar as notificações do navegador, deve haver um transporte de alerta referenciando este usuário',
         'enabled' => 'Notificações ativadas para este navegador',
         'disabled' => 'Notificações desativadas para este navegador',
