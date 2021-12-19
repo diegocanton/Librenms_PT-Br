06bf182ea4cf209e9f76a15888dd0fb3fa53630b
Diego Canton de Brito <diegocantonbgi@gmail.com>
2021-12-19 14:50:40 -0300

    TRADUÇÃO COMPLETA
    TRADUÇÃO COMPLETA.

--- pt-br/auth.php
+++ pt-br/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'title' => 'Auth',
+    'title' => 'Autenticação',
     'failed' => 'Credenciais não encontradas.',
     'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
 
