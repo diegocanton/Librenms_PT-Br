c52a3663e59001dff74c774d0f7a6fa3b5bbe3bb
Diego Canton de Brito <diegocantonbgi@gmail.com>
2021-12-19 14:57:52 -0300

    TRADUÇÃO COMPLETA

--- pt-br/plugins.php
+++ pt-br/plugins.php
@@ -2,11 +2,11 @@
 
 return [
     'settings_page' => 'Configurações :plugin:',
-    'admin_page' => 'Plugin Admin',
+    'admin_page' => 'Administração de Plugin',
     'admin_title' => 'Sistema de Plugins',
     'errors' => [
-        'not_exist' => 'Plugin :plugin não existe.',
-        'disabled' => 'Plugin :plugin está desativado.',
+        'not_exist' => 'O Plugin :plugin não existe.',
+        'disabled' => 'O Plugin :plugin está desativado.',
         'view_missing' => 'Visão ausente.',
     ],
 ];
